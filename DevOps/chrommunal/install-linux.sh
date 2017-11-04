1679f2aa7236368f451cdfe87b99966cc0fbc630
Wayne Li <lishixuan001@berkeley.edu>
2017-11-04 15:19:42 -0700

    work well til unzip the dawn for download

--- DevOps/chrommunal/install-linux.sh
+++ DevOps/chrommunal/install-linux.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
 #enable legacy boot
-sudo crossystem dev_boot_usb=1 dev_boot_legacy=1
+#sudo crossystem dev_boot_usb=1 dev_boot_legacy=1
 
 #flash firmware
-cd; curl -LO https://mrchromebox.tech/firmware-util.sh && sudo bash firmware-util.sh
+#cd; curl -LO https://mrchromebox.tech/firmware-util.sh && sudo bash firmware-#util.sh
 
 # Get Latest Dawn Version URL
 PYTHON_CMD="import json, sys"
 PYTHON_CMD="$PYTHON_CMD; assets = json.loads(sys.stdin.read())['assets']"
-PYTHON_CMD="$PYTHON_CMD; print([asset['browser_download_url'] for asset in assets if asset['name'].startswitch('dawn-linux-x64')][0])"
+PYTHON_CMD="$PYTHON_CMD; print([asset['browser_download_url'] for asset in assets if asset['name'].startswith('dawn-linux-x64')][0])"
 DAWN_URL=$(curl https://api.github.com/repos/pioneers/PieCentral/releases/latest | python -c "$PYTHON_CMD")
 
-wget $DAWN_URL -O /tmp/Dawn/dawn.zip
+# Download  Dawn to /tmp
+wget -O /tmp/dawn.zip $DAWN_URL 
 
 # Unzip dawn.zip to Desktop, replace if same name exist
-cd /tmp/Dawn
+cd /tmp
 unzip -o dawn.zip -d ~/Desktop
 
-# Clear /tmp/Dawn
-rm -rf /tmp/Dawn/*
+# Remove /tmp/dawn.zip
+rm -rf /tmp/dawn.zip
 
 # Install Dawn on Desktop
 
