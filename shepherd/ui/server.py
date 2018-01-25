c4463b62589e5e682dc1c77b5e0aa57c0164ec99
AW <andrewwan@pioneers.berkeley.edu>
2018-01-24 20:03:01 -0800

    LCM?

--- shepherd/ui/server.py
+++ shepherd/ui/server.py
@@ -3,6 +3,7 @@ import threading
 import time
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
+import gevent
 
 LCM_URL = "192.0.0.1"
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-RFID_list = '1|2|3|4|5|6'
+RFID_list = '1|2|3|4|5|0'
 
 @app.route('/')
 def hello_world():
@@ -25,11 +26,32 @@ def handle_message(message):
     print('received message: ' + message)
     if message == 'generate-rfid':
         print('sending-rfid')
-        socketio.emit('send-rfid', input('Input new RFID list'))
+        lcm_send(LCM_TARGETS.SHEPHERD, SHEPHERD_HEADER.GENERATE_RFID)
+        gevent.sleep()
+        socketio.emit('send-rfid', RFID_list)
 
 @socketio.on('join')
 def handle_message(client):
     print('confirmed join: ' + client)
+
+def send_message(str):
+    socketio.emit('message', str)
+    print('Should have sent')
+
+def receiver():
+
+    events = queue.Queue()
+    lcm_start_read(str.encode(LCM_TARGETS.UI), events)
+    while True:
+        event = events.get()
+        print("RECEIVED:", event)
+
+    #while True:
+        #gevent.sleep()
+        #send_message(input("Type something: "))
+
+t = threading.Thread(target=receiver)
+t.start()
 
 '''
 exit = True
