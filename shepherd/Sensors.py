52e247448d0c99d80845683325bf8e7f337edcb6
bodiddles <bdai87@gmail.com>
2018-01-31 20:21:37 -0800

    [SHEPHERD] Adds Threading to Sensors.py
    Sensors.py now spawns two threads reading from two specified comm ports
    and sending it over lcm with the specified alliance and goal.
    
    Message from serial port expects first 4 bytes representing the goal
    name, the 5th byte as the goal letter, and a new line character to
    denote the end of the packet.
    
    Requires integration testing with an actual device

--- shepherd/Sensors.py
+++ shepherd/Sensors.py
@@ -2,11 +2,33 @@ import queue
 from LCM import *
 from Utils import *
 import serial
+import threading
+import time
 
-gold_serial = serial.Serial("COM4", timeout = 0)
-blue_serial = serial.Serial("COM5", timeout = 0)
+port_one = "COM4"
+port_two = "COM5"
 
-while True:
-    lcm_send(LCM_TARGETS.SHEPHERD, SHEPHERD_HEADER.GOAL_SCORE, goal_letter, alliance)
-    gold_serial.read().decode("utf-8")
-    blue_serial.read().decode("utf-8")
+def transfer_sensor_data(ser):
+	while True:
+		sensor_msg = ser.readline().decode("utf-8")
+		alliance = sensor_msg[0:4].lower()
+		goal_letter = sensor_msg[4].lower()
+		lcm_send(LCM_TARGETS.SHEPHERD, SHEPHERD_HEADER.GOAL_SCORE, goal_letter, alliance)
+		time.sleep(0.01)
+
+goal_serial_one = serial.Serial(port_one)
+goal_serial_two = serial.Serial(port_two)
+
+goal_thread_one = threading.Thread(
+	target=transfer_sensor_data, name="transfer thread one", args=([goal_serial_one]))
+goal_thread_two = threading.Thread(
+	target=transfer_sensor_data, name="transfer thread two", args=([goal_serial_two]))
+goal_thread_one.daemon = True
+goal_thread_two.daemon = True
+
+goal_thread_one.start()
+goal_thread_two.start()
+
+if __name__ == "__main__":
+    while True:
+        time.sleep(10)
