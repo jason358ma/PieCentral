e1a7b0c17b49667997fb728e7896b20ac33d1ba0
Brandon Lee <brandonxxlee@gmail.com>
2018-02-10 14:08:05 -0800

    [SHEPHERD] Start implementation of bid station python code

--- shepherd/Sensors.py
+++ shepherd/Sensors.py
@@ -5,8 +5,10 @@ import serial
 from LCM import *
 from Utils import *
 
-port_one = "/dev/ttyACM0" # change to correct port
-port_two = "/dev/ttyACM1" # change to correct port
+sensor_port_one = "/dev/ttyACM0" # change to correct port
+sensor_port_two = "/dev/ttyACM1" # change to correct port
+bidding_port_one = "/dev/ttyACM2"
+bidding_port_two = "/dev/ttyACM3"
 
 alliance_mapping = {
     "gold": ALLIANCE_COLOR.GOLD,
@@ -29,7 +31,7 @@ def transfer_linebreak_data(ser):
         sensor_msg = ser.readline().decode("utf-8")
         if len(sensor_msg) != 7: #For Heartbeat
             continue
-        print("<2> Message Received: ", sensor_msg)
+        print("<2> Message Received: ", sensor_msg, flush=True)
         alliance = sensor_msg[0:4].lower()
         alliance_enum = alliance_mapping[alliance]
         goal_letter = sensor_msg[4].lower()
@@ -41,19 +43,38 @@ def transfer_linebreak_data(ser):
         lcm_send(LCM_TARGETS.SHEPHERD, SHEPHERD_HEADER.GOAL_SCORE, goal_enum, alliance_enum)
         time.sleep(0.01)
 
+def transfer_bid_data(ser):
+    print("<3> Starting Bidding Process", flush=True)
+    while True:
+        bid_msg = ser.readline().decode("utf-8")
+        time.sleep(0.01)
+
 def main():
-    goal_serial_one = serial.Serial(port_one)
-    goal_serial_two = serial.Serial(port_two)
+    goal_serial_one = serial.Serial(sensor_port_one)
+    goal_serial_two = serial.Serial(sensor_port_two)
 
     goal_thread_one = threading.Thread(
-        target=transfer_linebreak_data, name="transfer thread one", args=([goal_serial_one]))
+        target=transfer_linebreak_data, name="sensor transfer thread one", args=([goal_serial_one]))
     goal_thread_two = threading.Thread(
-        target=transfer_linebreak_data, name="transfer thread two", args=([goal_serial_two]))
+        target=transfer_linebreak_data, name="sensor transfer thread two", args=([goal_serial_two]))
     goal_thread_one.daemon = True
     goal_thread_two.daemon = True
 
     goal_thread_one.start()
     goal_thread_two.start()
+
+    bidding_serial_one = serial.Serial(bidding_port_one)
+    bidding_serial_two = serial.Serial(bidding_port_two)
+
+    bidding_thread_one = threading.Thread(
+        target=transfer_bid_data, name="bidding transfer thread one", args=([bidding_serial_one]))
+    bidding_thread_two = threading.Thread(
+        target=transfer_bid_data, name="bidding transfer thread two", args=([bidding_serial_two]))
+    bidding_thread_one.daemon = True
+    bidding_thread_two.daemon = True
+
+    bidding_thread_one.start()
+    bidding_thread_two.start()
 
     while True:
         time.sleep(100)
