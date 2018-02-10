2f3e0ceb26039ac4fd6dd7245881879eeccdafee
charleshong3 <charleshong@berkeley.edu>
2018-02-10 14:19:50 -0800

    Worked on scoreboard

--- shepherd/Scoreboard.js
+++ shepherd/Scoreboard.js
@@ -11,21 +11,32 @@ var grow = 0;
 // var increment=duration/pct;
 requestAnimationFrame(animate);
 
-function animate(time){
+function animate(time) {
     draw(pct);
     grow++;
     if (grow % 2 == 0) {
         pct++;
     }
-    if(pct<=endingPct){requestAnimationFrame(animate);}
+    if(pct<=endingPct) {
+        requestAnimationFrame(animate);
+    }
 }
 
 function draw(pct){
     var endRadians=-Math.PI/2+Math.PI*2*pct/100;
+    ctx.arc(150,125,120,-Math.PI/2,endRadians);
+    ctx.lineTo(150,125);
+    ctx.lineWidth = 15;
+    ctx.strokeStyle = 'green';
+    ctx.stroke();
+    // ctx.fillStyle='green';
+    // ctx.fill();
+    
     // ctx.fillStyle='white';
     // ctx.fillRect(0,0,cw,ch);
     // ctx.beginPath();
-    ctx.arc(150,125,100,-Math.PI/2,endRadians);
+    
+    ctx.arc(150,125,100,-Math.PI/2,endRadians, true);
     ctx.lineTo(150,125);
     ctx.fillStyle='blue';
     ctx.fill();
