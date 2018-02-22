e3795110ad5dfff7d5e0b00493b99b289666c74c
charleshong3 <charleshong@berkeley.edu>
2018-02-21 19:45:55 -0800

    Made everything into arcs

--- shepherd/Scoreboard.js
+++ shepherd/Scoreboard.js
@@ -13,33 +13,39 @@ requestAnimationFrame(animate);
 
 function animate(time) {
     draw(pct);
-    grow++;
-    if (grow % 2 == 0) {
-        pct++;
-    }
-    if(pct<=endingPct) {
+    pct += 0.5
+    if(pct<endingPct) {
         requestAnimationFrame(animate);
     }
 }
 
 function draw(pct){
     var endRadians=-Math.PI/2+Math.PI*2*pct/100;
-    ctx.arc(150,125,120,-Math.PI/2,endRadians);
-    ctx.lineTo(150,125);
-    ctx.lineWidth = 15;
+
+    ctx.fillStyle='white';
+    ctx.fillRect(0,0,cw,ch);
+    ctx.beginPath();
+    
+    ctx.arc(150,125,110,-Math.PI/2, endRadians, true);
+    ctx.moveTo(150,125);
+    ctx.lineWidth = 40;
     ctx.strokeStyle = 'green';
     ctx.stroke();
     // ctx.fillStyle='green';
     // ctx.fill();
-    
-    // ctx.fillStyle='white';
-    // ctx.fillRect(0,0,cw,ch);
-    // ctx.beginPath();
-    
-    ctx.arc(150,125,100,-Math.PI/2,endRadians, true);
-    ctx.lineTo(150,125);
-    ctx.fillStyle='blue';
-    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(150,125,45,-Math.PI/2, endRadians, true);
+    ctx.moveTo(150,125);
+    ctx.lineWidth = 90;
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
+
+    // ctx.arc(150,125,80,-Math.PI/2, endRadians, true);
+    // ctx.lineTo(150,125);
+    // ctx.fillStyle='blue';
+    // ctx.fill();
+
     // ctx.beginPath();
     // ctx.strokeStyle='#13a8a4';
     // ctx.lineJoin='bevel';
