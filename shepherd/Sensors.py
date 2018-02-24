a0bdc9886638334cc51569cea7dca4386e3d5c4a
baby-bell <baby-bell@users.noreply.github.com>
2018-02-24 14:06:38 -0800

    Detect sensor ports at runtime, instead of hardcoding

--- shepherd/Sensors.py
+++ shepherd/Sensors.py
@@ -23,6 +23,72 @@ goal_mapping = {
     "gg": GOAL.GOLD
 }
 
+IDENTIFY_TIMEOUT = 1
+
+def get_working_serial_ports(excludes: set):
+    """Get a list of working serial ports, excluding some.
+    
+    Returns a list of `serial.Serial` object.
+    """
+    import glob
+    maybe_ports = set(glob.glob("/dev/ttyACM*"))
+    maybe_ports.difference_update(excludes)
+
+    working = []
+    for p in maybe_ports:
+        try:
+            working.append(serial.Serial(p))
+        except serial.SerialException:
+            pass
+
+    return working
+
+
+def identify_linebreak_sensors(working_ports):
+    """ Check which ports have linebreak sensors on them.
+
+    Returns a list of serial ports.
+    """
+    def maybe_identify_sensor(serial_port, timeout, flag):
+        """Check whether a serial port contains a sensor.
+
+        Parameters:
+            serial_port -- the port to check
+            timeout -- quit reading from the serial port after this
+            amount of time
+            flag -- a flag to set if a device is successfully identified
+        """
+        prev_timeout = serial_port.timeout
+        serial_port.timeout = timeout
+        try:
+            msg = serial_port.readline().decode("utf-8")
+            if is_linebreak_sensor(msg):
+                flag.set()
+        except serial.SerialTimeoutException:
+            pass
+        serial_port.timeout = prev_timeout
+
+    flags = [threading.Event() for _ in working_ports]
+    threads = [threading.Thread(target=maybe_identify_sensor,
+                                args=(port,
+                                      IDENTIFY_TIMEOUT,
+                                      flag)) for (flag, port) in zip(flags, working_ports)]
+    for thread in threads:
+        thread.start()
+
+    time.sleep(IDENTIFY_TIMEOUT)
+    sensor_ports = [port for (flag, port) in zip(flags, working_ports) if flag.is_set()]
+    for thread in threads:
+        thread.join()
+    return sensor_ports
+
+
+def is_linebreak_sensor(sensor_msg):
+    """Check whether a message is sent from a linebreak sensor."""
+    # TODO: fill this in
+    pass
+
+
 def transfer_linebreak_data(ser):
     print("<1> Starting Linebreak Process", flush=True)
     while True:
