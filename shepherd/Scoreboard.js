819f6b424b1de25139f7548ba697601b743d655e
Kyle Su <kylesu98@berkeley.edu>
2018-03-03 15:02:00 -0800

    MORE CIRCLES

--- shepherd/Scoreboard.js
+++ shepherd/Scoreboard.js
@@ -9,14 +9,18 @@ var ctx4 = canvas4.getContext("2d");
 var canvas5 = document.getElementById("canvas5");
 var ctx5 = canvas5.getContext("2d");
 
+var contexts = [ctx, ctx2, ctx3, ctx4, ctx5];
+var pct = [0, 0, 0, 0, 0];
+var pct2 = [0, 0, 0, 0, 0];
+var pct3 = [0, 0, 0, 0, 0];
+var grow = [1, 1, 1, 1, 1];
+var grow2 = [1, 1, 1, 1, 1];
+var grow3 = [1, 1, 1, 1, 1];
+
 var cw=canvas.width;
 var ch=canvas.height;
 var duration=1;
 var endingPct=100;
-var pct = 0;
-var pct2 = 0;
-var grow = 1;
-var grow2 = 1;
 // var increment=duration/pct;
 var match_time = 300;
 var match_num = 0;
@@ -31,7 +35,6 @@ var gold_2_name = "gold 2";
 
 var bottom = document.getElementById("bottom_bar");
 var ctx_bottom = bottom.getContext("2d");
-
 setTeamsInfo();
 setTime();
 
@@ -80,52 +83,75 @@ function setTime() {
 
 function start(time){
     form = document.getElementById("seconds");
-    grow = parseFloat(form.elements[0].value)*10;
-    grow2 = parseFloat(form.elements[1].value)*10;
+    for(var i = 0; i < 5; i++){
+        grow[i] = parseFloat(form.elements[0].value.split(" ")[i])*10;
+        grow2[i] = parseFloat(form.elements[1].value.split(" ")[i])*10;
+        grow3[i] = parseFloat(form.elements[2].value.split(" ")[i])*10;
+    }
     beginning = new Date();
     function animate(){
         date = new Date();
         // pct += grow;
-        pct = (date - beginning)/grow;
-        pct2 = (date - beginning)/grow2;
-        draw(ctx, pct, pct2);
-        draw(ctx2, pct, pct2);
-        draw(ctx3, pct, pct2);
-        draw(ctx4, pct, pct2);
-        draw(ctx5, pct, pct2)
-        if(pct <= endingPct || pct2 <= endingPct){
-            requestAnimationFrame(animate);
+        for(var i = 0; i < 5; i++){
+            pct[i] = (date - beginning)/grow[i];
+            pct2[i] = (date - beginning)/grow2[i];
+            pct3[i] = (date - beginning)/grow3[i];
+        }
+        for(var i = 0; i < 5; i++){
+            draw(contexts[i], pct[i], pct2[i], pct3[i]);
+        }
+        // draw(ctx, pct, pct2);
+        // draw(ctx2, pct, pct2);
+        // draw(ctx3, pct, pct2);
+        // draw(ctx4, pct, pct2);
+        // draw(ctx5, pct, pct2);
+
+        for(var i = 0; i < 5; i++){
+            if(pct[i] <= endingPct || pct2[i] <= endingPct || pct3[i] <= endingPct){
+                requestAnimationFrame(animate);
+                break;
+            }
         }
     }
     requestAnimationFrame(animate);
 }
 
 
-function draw(ctx, pct, pct2) {
+function draw(ctx, pct, pct2, pct3) {
     var endRadians = -Math.PI/2 + Math.PI*2*pct/100;
     var endRadians2 = -Math.PI/2 + Math.PI*2*pct2/100;
+    var endRadians3 = -Math.PI/2 + Math.PI*2*pct3/100;
     ctx.fillStyle='white';
     ctx.fillRect(0,0,cw,ch);
     if (pct <= 100) {
         ctx.beginPath();
-        ctx.arc(150,125,90,-Math.PI/2,endRadians, true);
+        ctx.arc(150,125,100,-Math.PI/2,endRadians, true);
         ctx.moveTo(150,125);
-        ctx.strokeStyle='blue';
-        ctx.lineWidth = 30
+        ctx.strokeStyle='purple';
+        ctx.lineWidth = 20;
         ctx.stroke();
     }
     if (pct2 <= 100) {
         ctx.beginPath();
-        ctx.arc(150,125,70,-Math.PI/2,endRadians2, true);
-        ctx.lineTo(150,125);
-        ctx.fillStyle = 'goldenrod';
-        ctx.fill();
+        ctx.arc(150,125,80,-Math.PI/2,endRadians2, true);
+        ctx.moveTo(150,125);
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 20;
+        ctx.stroke();
     }
+    ctx.beginPath();
+    ctx.arc(150,125,60,-Math.PI/2,endRadians3, false);
+    ctx.lineTo(150,125);
+    ctx.fillStyle = 'goldenrod';
+    ctx.fill();
 }
 
 function addTime() {
-    grow += parseFloat(document.forms["addTime"].elements[0].value)*10;
-    grow2 += parseFloat(document.forms["addTime"].elements[1].value)*10;
+    for(var i = 0; i < 5; i++){
+        grow[i] += parseFloat(document.forms["addTime"].elements[0].value.split(" ")[i])*10;
+        grow2[i] += parseFloat(document.forms["addTime"].elements[1].value.split(" ")[i])*10;
+        grow3[i] += parseFloat(document.forms["addTime"].elements[2].value.split(" ")[i])*10;
+    }
     ctx.clearRect(0, 0, cw, ch);
     ctx.beginPath();
 }
